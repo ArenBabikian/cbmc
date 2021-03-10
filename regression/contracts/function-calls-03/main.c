8fca5e0a3b6bcbd2a0b0b0e2f85d501169de7118
Aren Babikian <babikian@dev-dsk-babikian-1d-e8327e0f.us-east-1.amazon.com>
2021-03-10 06:47:36 +0000

    Add 2 regression tests for recursive function calls

---
+++ regression/contracts/function-calls-03/main.c
@@ -0,0 +1,30 @@
+int f1(int *x1)
+  __CPROVER_requires(*x1 > 0 && *x1 < 20)
+  __CPROVER_ensures(__CPROVER_return_value == *x1+ 2);
+
+//Note 1: no way to check that the requires clause needs to have "*x2 >= 0" and not "*x2 > 0" due to the recursion
+int f2(int *x2)
+  __CPROVER_requires(*x2 >= 0 && *x2 < 20)
+  __CPROVER_ensures(__CPROVER_return_value == *x2+1);
+
+int f1(int *x1)
+{
+  return f2(x1) + 1;
+}
+
+//Note 2: impossible to "check" f2 by "checking" the outer function and "replacing" the internal recursive call.
+int f2(int *x2)
+{
+  if (*x2 < 1) return 1;
+  int loc2 = *x2-1;
+  return f2(&loc2) + 1;
+}
+
+int main()
+{
+  int p ; 
+  __CPROVER_assume(p > 0 && p < 20);
+  f1(&p);
+
+  return 0;
+}
